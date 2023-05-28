3e9c7857c17f3ef22e2fe4d0296d90c5c2d71967
MoosaAfzal <88625853+MoosaAfzal@users.noreply.github.com>
2023-05-29 02:55:51 +0500

    Update Quantity.tsx

--- app/shared/Quantity.tsx
+++ app/shared/Quantity.tsx
@@ -3,13 +3,11 @@ import { useState } from "react"
 
 const Quantity = () => {
     const [Quantity, setQuantity] = useState(1)
-    console.log(Quantity);
-
     return (
         <div className="flex items-center gap-3">
-            <div onClick={() => { if (Quantity !== 1) { setQuantity(Quantity - 1) } }} className="text-2xl px-3.5 py-1 bg-[#f1f1f1] rounded-full">-</div>
+            <div onClick={() => { if (Quantity > 1) { setQuantity(Quantity - 1) } }} className="text-2xl px-3.5 py-1 bg-[#f1f1f1] rounded-full cursor-pointer">-</div>
             {Quantity}
-            <div onClick={() => { if (Quantity !== 1) { setQuantity(Quantity + 1) } }} className="text-2xl px-3.5 py-1 bg-[#f1f1f1] rounded-full">+</div>
+            <div onClick={() => setQuantity(Quantity + 1)} className="text-2xl px-3.5 py-1 bg-[#f1f1f1] rounded-full cursor-pointer">+</div>
         </div>
     )
 }
